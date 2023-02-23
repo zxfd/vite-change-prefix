3ae71d9b50d98e5389bf9659036e1135ae629683
八宝粥 <zxiaofoo@qq.com>
2023-02-23 14:55:48 +0800

    feat(rollup): ✨ 更新打包导出配置

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,14 @@
 export default {
   input: "./src/index.js",
   external: ["@rollup/plugin-replace"],
-  output: {
-    format: "es",
-    file: "lib/index.esm.js",
-    name: "bundle-name",
-  },
+  output: [
+    {
+      format: "es",
+      file: "lib/es/index.mjs",
+    },
+    {
+      format: "cjs",
+      file: "lib/cjs/index.cjs",
+    },
+  ],
 }
